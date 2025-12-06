518dbbec070bf8a2f769a7ce6447d9bc0dff3ec8
Kevin! <kevinsehnem123@gmail.com>
2025-12-06 10:21:42 -0300

    subindo a build corretamente para o gitpages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@
 
   export default defineConfig({
     plugins: [react()],
+    base: '/SGHSS-Uninter/', // muito importante!
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
       alias: {
